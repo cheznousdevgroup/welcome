1d7954d63af944d80a322fd7b1c63917406fd5a6
Chez nous dev Group <122475519+cheznousdevgroup@users.noreply.github.com>
2023-02-21 21:59:52 +0100

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<!-- Le pied de la page -->
-<footer>
-            <p class="auteur">Author: C. nous Dev Group</p>
-            <p><a href="mailto:C.nousdevgroup@gmail.com">C.nousdevgroup@gmail.com</a></p>
-</footer>   
-</body>
-<!-- Le script de notre menu Hamberger -->
-<script src="https://kit.fontawesome.com/67c66657c7.js"></script>
-</html>
